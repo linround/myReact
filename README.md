2aae03ef4186cbdbaf86e5f52c18d7dea5216010
linyuan1105 <673609957@qq.com>
2023-11-22 22:41:10 +0800

    feat: build jsx react

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 - commit 校验 husky
 - 代码格式校验 eslint
 - 打包工具 rollup
+## package.json
+- main 对应的commonjs 规范的入口
+- module 对应的是 esmodule 规范的入口
---
+++ packages/shared/ReactSymbols.ts
@@ -0,0 +1,2 @@
+const supportSymbol = typeof Symbol=== 'function'
+export const REACT_ELEMENT_TYPE = supportSymbol?Symbol('react.element'):0xeac7
