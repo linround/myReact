0dd12537d56751ea9a96d3c01d0a4a3c78137102
linyuan1105 <673609957@qq.com>
2023-12-24 14:44:56 +0800

    feat:  react 16-1

---
+++ packages/react-noop=render/src/root.ts
@@ -0,0 +1,18 @@
+// ReactDOM.createRoot(root).render(<App />)
+import { Container } from 'hostConfig';
+import {
+	createContainer,
+	updateContainer
+} from 'react-reconciler/src/fiberReconciler';
+import { ReactElementType } from 'shared/ReactTypes';
+import { initEvent } from './SyntheticEvent';
+
+export function createRoot(container: Container) {
+	const root = createContainer(container);
+	return {
+		render(element: ReactElementType) {
+			initEvent(container, 'click');
+			return updateContainer(element, root);
+		}
+	};
+}
