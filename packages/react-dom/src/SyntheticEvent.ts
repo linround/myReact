6d4636e604f7820c1b2af76f9e6cd8e446628171
linyuan1105 <673609957@qq.com>
2024-01-07 15:29:13 +0800

    feat:  react 18-1

--- packages/react-dom/src/SyntheticEvent.ts
+++ packages/react-dom/src/SyntheticEvent.ts
@@ -2,6 +2,12 @@
 
 import { Props } from 'shared/ReactTypes';
 import { Container } from 'hostConfig';
+import {
+	unstable_ImmediatePriority,
+	unstable_NormalPriority,
+	unstable_runWithPriority,
+	unstable_UserBlockingPriority
+} from 'scheduler';
 
 export const elementPropsKey = '__props';
 // 合法的事件系统
@@ -75,7 +81,10 @@ function dispatchEvent(container: Container, eventType: string, e: Event) {
 function triggerEventFlow(paths: EventCallback[], se: SyntheticEvent) {
 	for (let i = 0; i < paths.length; i++) {
 		const callback = paths[i];
-		callback.call(null, se);
+
+		unstable_runWithPriority(eventTypeToSchedulerPriority(se.type), () => {
+			callback.call(null, se);
+		});
 		if (se.__stopPropagation) {
 			break;
 		}
@@ -122,4 +131,17 @@ function collectPaths(
 	}
 
 	return paths;
+}
+
+function eventTypeToSchedulerPriority(eventType: string) {
+	switch (eventType) {
+		case 'click':
+		case 'keydown':
+		case 'keyup':
+			return unstable_ImmediatePriority;
+		case 'scroll':
+			return unstable_UserBlockingPriority;
+		default:
+			return unstable_NormalPriority;
+	}
 }
--- packages/react-reconciler/src/fiberLanes.ts
+++ packages/react-reconciler/src/fiberLanes.ts
@@ -1,17 +1,36 @@
 import { FiberRootNode } from './fiber';
+import {
+	unstable_getCurrentPriorityLevel,
+	unstable_IdlePriority,
+	unstable_ImmediatePriority,
+	unstable_NormalPriority,
+	unstable_UserBlockingPriority
+} from 'scheduler';
+import { sync } from 'rimraf';
 
 export type Lane = number;
 export type Lanes = number;
 export const SyncLane = 0b0001;
 export const NoLane = 0b0000;
 export const NoLanes = 0b0000;
+export const InputContinuousLane = 0b0010; // 连续的输入事件
+export const DefaultLane = 0b0100;
+export const IdleLane = 0b1000;
 
 export function mergeLane(laneA: Lane, laneB: Lane): Lanes {
 	return laneA | laneB;
 }
 
+// 运行流程在react 时，使用的时 lane 模型
+// 运行流程在 scheduler 时，使用的是优先级
+
+// 所以需要实现两者的转换
 export function requestUpdateLanes() {
-	return SyncLane;
+	// 从上下文环境中获取 scheduler 优先级
+	const currentSchedulerPriority = unstable_getCurrentPriorityLevel();
+	const lane = schedulerPriorityToLane(currentSchedulerPriority);
+
+	return lane;
 }
 
 export function getHighestPriorityLane(lanes: Lanes) {
@@ -20,4 +39,32 @@ export function getHighestPriorityLane(lanes: Lanes) {
 
 export function markRootFinished(root: FiberRootNode, lane: Lane) {
 	root.pendingLanes &= ~lane;
+}
+
+function laneToSchedulerPriority(lanes: Lanes) {
+	const lane = getHighestPriorityLane(lanes);
+
+	if (lane === SyncLane) {
+		return unstable_ImmediatePriority;
+	}
+	if (lane === InputContinuousLane) {
+		return unstable_UserBlockingPriority;
+	}
+	if (lane === DefaultLane) {
+		return unstable_NormalPriority;
+	}
+	return unstable_IdlePriority;
+}
+
+function schedulerPriorityToLane(schedulerPriority: number): Lane {
+	if (schedulerPriority === unstable_ImmediatePriority) {
+		return SyncLane;
+	}
+	if (schedulerPriority === unstable_UserBlockingPriority) {
+		return InputContinuousLane;
+	}
+	if (schedulerPriority === unstable_NormalPriority) {
+		return DefaultLane;
+	}
+	return NoLane;
 }
