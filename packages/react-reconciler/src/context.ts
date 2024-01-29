102cd8a082fdcd329d65b38f0ffa3c6667aefff1
linyuan1105 <673609957@qq.com>
2024-01-29 12:17:28 +0800

    feat:  react 21-1

---
+++ packages/react-reconciler/src/context.ts
@@ -0,0 +1,18 @@
+import {
+	REACT_CONTEXT_TYPE,
+	REACT_PROVIDER_TYPE,
+	ReactContext
+} from 'shared/ReactSymbols';
+
+export function createContext<T>(defaultValue: T): ReactContext<T> {
+	const context: ReactContext<T> = {
+		$$typeof: REACT_CONTEXT_TYPE,
+		Provider: null,
+		_currentValue: defaultValue
+	};
+	context.Provider = {
+		$$typeof: REACT_PROVIDER_TYPE,
+		_context: context
+	};
+	return context;
+}
--- packages/shared/ReactSymbols.ts
+++ packages/shared/ReactSymbols.ts
@@ -1,3 +1,16 @@
 const supportSymbol = typeof Symbol === 'function';
 export const REACT_ELEMENT_TYPE = 0xeac7;
 export const REACT_FRAGMENT_TYPE = 0xeacb;
+export const REACT_CONTEXT_TYPE = 0xeacc;
+export const REACT_PROVIDER_TYPE = 0xeac2;
+
+export type ReactContext<T> = {
+	$$typeof: symbol | number;
+	Provider: ReactProviderType<T> | null;
+	_currentValue: T;
+};
+
+export type ReactProviderType<T> = {
+	$$typeof: symbol | number;
+	_context: ReactContext<T> | null;
+};
